b7f37597bced2474e2ec8033d6ab9602496637d0
Sarah Armitage <saraharmitage314@gmail.com>
2020-03-13 19:04:10 -0400

    Updates

---
+++ precache-manifest.385364231ffe0c00ad0e1a9587c5d0aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042caf2e2f809ddc00a355ea7f363c14",
+    "url": "/image-finder/index.html"
+  },
+  {
+    "revision": "ae59ff784604ffb6cbf2",
+    "url": "/image-finder/static/css/main.6432650f.chunk.css"
+  },
+  {
+    "revision": "07e65e0568e52110b88c",
+    "url": "/image-finder/static/js/2.ece8d6d7.chunk.js"
+  },
+  {
+    "revision": "ae59ff784604ffb6cbf2",
+    "url": "/image-finder/static/js/main.abb7f7cc.chunk.js"
+  },
+  {
+    "revision": "ab3d409e4e3070c76e36",
+    "url": "/image-finder/static/js/runtime-main.6d4565bc.js"
+  }
+]);
